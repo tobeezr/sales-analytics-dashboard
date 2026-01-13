88ff9c58d60264a15578e84f7c80e41feed2d48b
tobeez <younessbellasri@gmail.com>
2026-01-13 15:52:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 from datetime import datetime, timedelta
-import warnings
-warnings.filterwarnings('ignore')
 
 # ============================================================================
 # PAGE CONFIG
